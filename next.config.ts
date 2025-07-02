6b77a630690e1c4496d7ba858834f36cbda26b4f
divineninja <junreyjr1029@gmail.com>
2025-07-02 16:57:00 +0800

    feat: update configuration for deployment and add necessary dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+
+const repoName = "porfolio-website"; // GitHub repo name
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
